e0a605da1eb93da54287b742c894acba9f50f63d
Jamon Camisso <jamon@chainguard.dev>
2024-02-27 21:40:36 +0000

    kafka-ui sans-tests

--- images/kafka-ui/tests/main.tf
+++ images/kafka-ui/tests/main.tf
@@ -23,25 +23,25 @@ data "oci_string" "ref" {
 
 resource "random_pet" "suffix" {}
 
-resource "helm_release" "kafka-ui" {
-  name             = "kafka-ui-${random_pet.suffix.id}"
-  namespace        = "kafka-ui-${random_pet.suffix.id}"
-  repository       = "https://provectus.github.io/kafka-ui-charts"
-  chart            = "kafka-ui"
-  create_namespace = true
+#resource "helm_release" "kafka-ui" {
+#  name             = "kafka-ui-${random_pet.suffix.id}"
+#  namespace        = "kafka-ui-${random_pet.suffix.id}"
+#  repository       = "https://provectus.github.io/kafka-ui-charts"
+#  chart            = "kafka-ui"
+#  create_namespace = true
+#
+#  values = [
+#    jsonencode({
+#      image = {
+#        repository = data.oci_string.ref.registry_repo
+#        tag        = data.oci_string.ref.pseudo_tag
+#      }
+#    })
+#  ]
+#}
 
-  values = [
-    jsonencode({
-      image = {
-        repository = data.oci_string.ref.registry_repo
-        tag        = data.oci_string.ref.pseudo_tag
-      }
-    })
-  ]
-}
-
-module "helm_cleanup" {
-  source    = "../../../tflib/helm-cleanup"
-  name      = helm_release.kafka-ui.id
-  namespace = helm_release.kafka-ui.namespace
-}
+#module "helm_cleanup" {
+#  source    = "../../../tflib/helm-cleanup"
+#  name      = helm_release.kafka-ui.id
+#  namespace = helm_release.kafka-ui.namespace
+#}
